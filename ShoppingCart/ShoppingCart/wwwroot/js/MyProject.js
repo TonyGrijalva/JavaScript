0e0eb4b74ee2d22fb9e1a011c5b78e20dab89b41
Marcos Grijalva <shonish04@gmail.com>
2021-02-19 19:37:51 -0700

    Week 1 assignments first attempt

--- ShoppingCart/ShoppingCart/wwwroot/js/MyProject.js
+++ ShoppingCart/ShoppingCart/wwwroot/js/MyProject.js
@@ -1,1 +1,41 @@
-﻿console.log(2);
+﻿var s = typeof x; 
+var t = null;
+var u = 3e3;
+var v = 3.24;
+var w = true;
+var x = 2;
+//var y = 10;
+
+var y = "I can't write";
+var z = 'This is a quote';
+//var myVariable = document.getElementById("MySpan2");
+//var w = true;
+
+console.log(z);
+//console.log(typeof x);
+//console.log(myVariable);
+//console.log(y);
+//console.log(z);
+//console.log(123456);
+//console.log("string");
+
+var a = 0;
+var b = 1;
+var c = 2;
+
+//function myFunction(x, y, z)() {
+  //  console.log(x);
+    //console.log(y);
+    //console.log(z);
+//}
+
+//xmyFunction(34, 2, 4);
+
+
+function myFunction(x, y, z) {
+    return x + y + z;
+}
+
+var result = myFunction(432434, "b", false);
+
+console.log(result);
